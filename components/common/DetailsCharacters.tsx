db34add4d8f392728a7a3c93d572eda62236ec1c
Harim Zermeno <harimzermeno@Harims-MacBook-Pro.local>
2023-10-20 21:10:55 -0700

    Bug fig

--- components/common/DetailsCharacters.tsx
+++ components/common/DetailsCharacters.tsx
@@ -1,8 +1,6 @@
 import { AnimeCharactersData } from "@/types";
 import React from "react";
 import { CharacterCard } from "../ui";
-import { useRouter } from "next/navigation";
-import { useParams } from "next/navigation";
 
 interface Props {
   data: AnimeCharactersData | null;
@@ -11,13 +9,10 @@ interface Props {
 
 const DetailsCharacters = ({ data, max }: Props) => {
   const characters = max ? data?.data.slice(0, max) : data?.data;
-  const router = useParams();
 
   if (!data) {
     return "Loading...";
   }
-
-  console.log(data);
 
   return (
     <>
@@ -25,7 +20,7 @@ const DetailsCharacters = ({ data, max }: Props) => {
         <h2 className="font-semibold text-gray-400 mb-[1rem]">Characters</h2>
       )}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-[2rem] auto-rows-min mb-[3rem]">
-        {characters?.map((character) => (
+        {characters?.map((character: (typeof data.data)[0]) => (
           <CharacterCard
             key={character.character.mal_id}
             character={character}
