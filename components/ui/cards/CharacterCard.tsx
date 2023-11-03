7d7c65791b0f3c0d0232e910ccf7f59d2aba33e9
Harim Zermeno <harimzermeno@Harims-MacBook-Pro.local>
2023-11-03 13:17:50 -0700

    Moved types

--- components/ui/cards/CharacterCard.tsx
+++ components/ui/cards/CharacterCard.tsx
@@ -1,6 +1,37 @@
 import Image from "next/image";
 import React from "react";
-import { type AnimeCharacter } from "@/types/AnimeCharacters";
+
+type AnimeCharacter = {
+  character: {
+    mal_id: number;
+    url: string;
+    images: {
+      jpg: {
+        image_url: string;
+        small_image_url: string;
+      };
+      webp: {
+        image_url: string;
+        small_image_url: string;
+      };
+    };
+    name: string;
+  };
+  role: string;
+  voice_actors: {
+    person: {
+      mal_id: number;
+      url: string;
+      images: {
+        jpg: {
+          image_url: string;
+        };
+      };
+      name: string;
+    };
+    language: string;
+  }[];
+};
 
 interface Props {
   character: AnimeCharacter | null;
